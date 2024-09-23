a7cca1ed94b1490b3d693349f80c5f53cefef97e
Gupta-deepak1906 <147096327+Gupta-deepak1906@users.noreply.github.com>
2024-09-24 01:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const inputField = document.getElementById("fname");
+
+inputField.addEventListener('burr',()=>{
+	inputField.value=inputField.value.toUpperCase();
+});
